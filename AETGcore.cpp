28e1753e3b986cf8be8ffd1c90ae5be59d84591b
Kouek <598601889@qq.com>
2020-11-24 23:44:50 +0800

    修复若干Bug

--- AETGcore.cpp
+++ AETGcore.cpp
@@ -78,15 +78,10 @@ struct Cover
 	~Cover() {
 		delete[] dat;
 	}
-	int idxOf(int v) const {
-		for (int i = 0; i < T; i++)
-			if (dat[i] == v)return i;
-		return -1;
-	}
 	void insert(int v) {
 		dat[size++] = v;
 
-		// Cover满时排序
+		// Cover满T维时排序
 		if (size == T)
 			qsort(dat, T, sizeof(int), cmpINT);
 	}
@@ -117,6 +112,11 @@ static int* seq; // 表示K个测试项的序列
 static int* maxAppear; // 表示每个取值的出现次数
 static RandSeq* randSeq; // 用于产生随机下标序列
 static int* realSeq; // 用于恢复随机打乱后的下标序列顺序
+
+// 用于寻找下一下标对应取值中，K<T部分的计算
+//   因为配对深度2<= T <=10，所以只需2,3,..,9深度，对应数组大小8
+//   key:覆盖 val:覆盖的出现次数
+static unordered_map<Cover, int, CoverHash, CoverEq> CoversetOfLessT[8];
 
 void genAllCov(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int T, int K, const int* V, int** val)
 {
@@ -154,7 +154,7 @@ void genAllCov(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int T, int K,
 					tmpidxs.pop();
 				}
 				// }
-				ALL[Cover(cov)] = 0; // cov加入ALL
+				ALL[Cover(cov)] = 0; // 将cov加入ALL，使用深度拷贝防止dat被delete，初始状态为0
 
 				if (idxs.top() < V[seq[t - 1]] - 1) // 当前叶子节点还有弟，继续遍历弟
 				{
@@ -210,6 +210,7 @@ void coverWithRow(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int* uncov
 	for (i = 0; i < K; i++)
 		seq[i] = i;
 
+	// 将ALL中被ROW覆盖的状态设为1
 	Cover cov(T);
 	do {
 		cov.clear();
@@ -219,24 +220,24 @@ void coverWithRow(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int* uncov
 		{
 			ALL[cov] = 1; (*uncoverNum)--;
 		}
-	} while (next_comb(seq, K, T));
+	} while (next_comb(seq, K, T)); // 在K位中取出T位进行组合
 }
 
 void genFirstRow(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int* uncoverNum, 
 	vector<vector<int>>& TC,
 	int T, int K, int** val)
 {
+	// 所有测试项的第一个取值构成第一行用例
 	vector<int> firstRow;
 	for (int i = 0; i < K; i++)
 		firstRow.push_back(val[i][0]);
 
 	int oriUncoverNum = *uncoverNum;
-	coverWithRow(ALL, uncoverNum, firstRow, T, K);
-
+	coverWithRow(ALL, uncoverNum, firstRow, T, K); // 让firstRow覆盖ALL
 	TC.push_back(firstRow);
-	TC[TC.size() - 1].push_back(oriUncoverNum - *uncoverNum);
+	TC[TC.size() - 1].push_back(oriUncoverNum - *uncoverNum); // 记载当前行的覆盖数
 
-	qDebug() << *uncoverNum << "left" << oriUncoverNum - *uncoverNum << "minused";
+	qDebug() << *uncoverNum << "left" << oriUncoverNum - *uncoverNum << "minused"; // Debug
 }
 
 void countAppearInUncover(const unordered_map<Cover, int, CoverHash, CoverEq>& ALL,
@@ -249,7 +250,8 @@ void countAppearInUncover(const unordered_map<Cover, int, CoverHash, CoverEq>& A
 	for (auto itr = ALL.begin(); itr != ALL.end(); itr++)
 		if (itr->second == 0)
 			for (i = 0; i < T; i++)
-				maxAppear[itr->first.dat[i]]++;
+				if (itr->first.dat[i] >= 0 && itr->first.dat[i] < V[K - 1] + val[K - 1][0]) // 不知道为什么会越界，所以保护一下
+					maxAppear[itr->first.dat[i]]++;
 }
 
 int genNextElem(unordered_map<Cover, int, CoverHash, CoverEq>& ALL,
@@ -258,29 +260,26 @@ int genNextElem(unordered_map<Cover, int, CoverHash, CoverEq>& ALL,
 {
 	if (K < T)
 	{
-		int coverNum, maxOff = 0, maxCoverNum = 0, flag;
-		ROW.push_back(val[idx][maxOff]);
+		int i, maxOff = 0, maxCoverNum = 0, findFag = 0;
+		Cover cov(T);
+		ROW.push_back(val[idx][0]);
+
 		for (int j = 0; j < V[idx]; j++)
 		{
 			ROW[ROW.size() - 1] = val[idx][j];
-			coverNum = 0;
-			for (auto itr = ALL.begin(); itr != ALL.end(); itr++)
-			{
-				flag = 1;
-				if (itr->second == 0)
-					for (auto itr2 = ALL.begin(); itr2 != ALL.end(); itr2++)
-						if (itr->first.idxOf(val[idx][j]) == -1)
-						{
-							flag = 0; break;
-						}
-				if (flag)coverNum++;
-			}
-			if (coverNum > maxCoverNum)
-			{
-				maxOff = j;
-				maxCoverNum = coverNum;
-			}
+			cov.clear();
+			for (i = 0; i < K; i++)
+				cov.insert(ROW[i]);
+			if (CoversetOfLessT[K - 2].find(cov) != CoversetOfLessT[K - 2].end())
+				if (CoversetOfLessT[K - 2][cov] > maxCoverNum)
+				{
+					maxOff = j; findFag = 1;
+				}
 		}
+
+		if (!findFag)
+			maxOff = floor(1.0f * V[idx] * rand() / RAND_MAX);
+
 		ROW[ROW.size() - 1] = val[idx][maxOff];
 		return 0;
 	}
@@ -316,13 +315,34 @@ int genNextElem(unordered_map<Cover, int, CoverHash, CoverEq>& ALL,
 	}
 }
 
+void countCovAppearInUncover(const unordered_map<Cover, int, CoverHash, CoverEq>& ALL,
+	int T, int K)
+{
+	Cover cov(T);
+	CoversetOfLessT[K - 2].clear();
+	for (auto itr = ALL.begin(); itr != ALL.end(); itr++)
+		if (itr->second == 0)
+		{
+			do {
+				cov.clear();
+				for (int i = 0; i < T; i++)
+					cov.insert(itr->first.dat[i]);
+				if (CoversetOfLessT[K - 2].find(cov) == CoversetOfLessT[K - 2].end())
+					CoversetOfLessT[K - 2][Cover(cov)] = 1;
+				else
+					CoversetOfLessT[K - 2][cov]++;
+			} while (next_comb(seq, T, K));
+		}
+}
+
 void genNextRow(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int* uncoverNum,
 	vector<vector<int>>& TC,
 	int T, int K, const int* V, int** val, int M)
 {
 	int maxIdx = 0, maxOff = 0, maxV = 0, start;
-	vector<int> nextRow(K + 1);
+	vector<int> nextRow(K + 1); // 最后一位用于表示每行用例的覆盖数
 	
+	// 选取第一个取值
 	int i, j;
 	countAppearInUncover(ALL, T, K, V, val);
 	for (i = 0; i < K; i++)
@@ -331,34 +351,42 @@ void genNextRow(unordered_map<Cover, int, CoverHash, CoverEq>& ALL, int* uncover
 			{
 				maxIdx = i; maxOff = j;
 			}
-	TC.push_back(nextRow); // 将队尾作为临时储存变量，储存最大覆盖的row
+	
+	// 从当前ALLL中未覆盖的T覆盖中选i个，统计未覆盖的i覆盖数量，存入CoversetOfLessT[i-2]
+	for (i = 2; i < T; i++)
+		countCovAppearInUncover(ALL, T, i);
 
+	// 尝试M次，选出M个结果候选中覆盖最多的用例
+	TC.push_back(nextRow); // 将队尾作为临时储存变量，储存最优用例
 	int k, coverNum = 0;
 	maxV = 0;
+	realSeq[0] = maxIdx; // 用于恢复原有下标顺序
 	for (int m = 0; m < M; m++)
 	{
 		nextRow.clear();
-		nextRow.push_back(val[maxIdx][maxOff]);
+		nextRow.push_back(val[maxIdx][maxOff]); // 加入第一个取值
 		resetRandSeq(randSeq, K);
 		for (k = 1; k < K; k++)
 		{
-			i = next_rand_idx(randSeq, K - k, maxIdx);
-			realSeq[k] = i;
-			coverNum = genNextElem(ALL, nextRow, T, k + 1, V, val, i);
+			i = next_rand_idx(randSeq, K - k, maxIdx); // 随机得到下一个待处理的下标
+			realSeq[k] = i; // 用于恢复原有下标顺序
+			coverNum = genNextElem(ALL, nextRow, T, k + 1, V, val, i); // 选出下一个下标对应的取值，返回覆盖数
 		}
 
+		// 用于获取最优用例
 		if (coverNum > maxV)
 		{
 			maxV = coverNum;
-			TC[TC.size() - 1] = nextRow;
+			for (i = 0; i < K; i++)
+				TC[TC.size() - 1][realSeq[i]] = nextRow[i]; // 恢复nextRow为原有下标并加入TC
 		}
 	}
 	
 	int oriUncoverNum = *uncoverNum;
-	coverWithRow(ALL, uncoverNum, TC[TC.size() - 1], T, K);
-	TC[TC.size() - 1].push_back(oriUncoverNum - *uncoverNum);
+	coverWithRow(ALL, uncoverNum, TC[TC.size() - 1], T, K); // 让新产生的行覆盖ALL
+	TC[TC.size() - 1][K] = oriUncoverNum - *uncoverNum; // 记载当前行的覆盖数
 
-	qDebug() << *uncoverNum << "left" << oriUncoverNum - *uncoverNum << "minused";
+	qDebug() << *uncoverNum << "left" << oriUncoverNum - *uncoverNum << "minused"; // Debug
 }
 
 void AETG(vector<vector<int>>& TC, int T, int K, int* V, int M)
@@ -367,7 +395,7 @@ void AETG(vector<vector<int>>& TC, int T, int K, int* V, int M)
 	//   key:覆盖 val:覆盖的状态{0:未覆盖, 1:覆盖}
 	unordered_map<Cover, int, CoverHash, CoverEq> ALL;
 
-	int sum, i;
+	int sum, i, j;
 	seq = new int[K]; // 表示K个测试项的序列
 	randSeq = new RandSeq[K]; // 用于产生随机下标序列
 	realSeq = new int[K]; // 用于恢复随机打乱后的下标序列顺序
@@ -378,7 +406,6 @@ void AETG(vector<vector<int>>& TC, int T, int K, int* V, int M)
 	// seq={0,1,2}
 	// val={{0},{1,2},{3,4,5}}
 	{
-		int j;
 		val = new int* [K];
 		for (i = 0, sum = 0; i < K; i++)
 		{
@@ -389,14 +416,14 @@ void AETG(vector<vector<int>>& TC, int T, int K, int* V, int M)
 	}
 	maxAppear = new int[sum]; // 表示每个取值的出现次数
 
-	genAllCov(ALL, T, K, V, val);
+	genAllCov(ALL, T, K, V, val); // 第一步，计算所有从K个正交测试项各自的取值中取出T个，得到的覆盖集合ALL
 
 	int uncoverNum = ALL.size();
-	genFirstRow(ALL, &uncoverNum, TC, T, K, val);
-	while (uncoverNum)
-		genNextRow(ALL, &uncoverNum, TC, T, K, V, val, M);
+	genFirstRow(ALL, &uncoverNum, TC, T, K, val); // 第二步，产生第一行用例
+	while (uncoverNum > 0)
+		genNextRow(ALL, &uncoverNum, TC, T, K, V, val, M); // 第三步，按AETG算法产生剩下的用例
 
-	// 释放
+	// 释放内存
 	{
 		delete[] seq;
 		delete[] maxAppear;
